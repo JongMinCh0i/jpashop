54f5bdcc7128b6608ff1d3d2d19401d9b75b063f
JongMinCh0i <wonderland314592@gmail.com>
2023-09-29 14:13:38 +0900

    3.3 회원 기능 테스트

---
+++ src/test/java/jpabook/jpashop/domain/repository/MemberRepositoryTest.java
@@ -0,0 +1,64 @@
+package jpabook.jpashop.domain.repository;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.service.MemberService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
+
+
+    @Test
+//    @Rollback(false) 눈으로 확인할 때 사용
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        em.flush(); // 영속성 컨텍스트에서 DB에 쿼리를 날린다.
+        Assertions.assertEquals(member, memberRepository.findOne(savedId));
+
+    }
+
+    @Test
+    void 중복_회원_예외() throws Exception {
+
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+
+        //then
+        assertThatThrownBy(() -> memberService.join(member2))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+}
